08835164db09d2034588ef0d72ed1c1d45ca16df
yogesh142 <30934011+yogesh142@users.noreply.github.com>
2017-08-11 22:31:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one', function (req, res) {
+    res.send('Article one is servered here');
+});
+
+app.get('/article-two', function (req, res) {
+    res.send('Article two is servered here');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
